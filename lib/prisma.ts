62be95fff15fc07e3ced9ebc155b38cafad52ee7
Salisu Iliyasu <salisuiliyasu101@gmail.com>
2025-10-21 11:25:32 +0100

    Fix Prisma generate before build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@/lib/generated/prisma";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
